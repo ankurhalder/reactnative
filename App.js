39caa7aef4889a832c9c78a9e78db18133afbac6
Ankur Halder <ankur.halder12345@gmail.com>
2025-01-29 15:48:37 +0530

    feat: enhance App component layout with styled input field and button

--- App.js
+++ App.js
@@ -2,15 +2,32 @@ import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
   return (
-    <View>
-      <View>
-        <TextInput placeholder="Your course goal!" />
+    <View style={styles.appContainer}>
+      <View style={styles.imputContainer}>
+        <TextInput style={styles.TextInput} placeholder="Your course goal!" />
         <Button title="Add Goal" />
+      </View>
+      <View>
         <Text>List of goals</Text>
       </View>
-      <View></View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  appContainer: {
+    padding: 50,
+  },
+  imputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+
+  TextInput: {
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    width: "80%",
+    marginRight: 8,
+    padding: 8,
+  },
+});
